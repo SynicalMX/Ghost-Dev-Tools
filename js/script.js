d95d02bcf7603ec4bb414ec32b3ed06187b4fa9b
SynicalMX <70176240+SynicalMX@users.noreply.github.com>
2022-11-15 13:26:03 -0500

    added connect

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 const idinput = document.getElementById("id-input")
 const connect = document.getElementById("connect")
 
+const socket = new WebSocket("10.201.102.239:3000")
+
+const template = {
+    method: "connect",
+    params: {
+        id: idinput.value,
+    }
+}
+
 connect.onclick = () => {
-	
+    socket.send(template)
+    socket.onmessage = (event) => {
+        console.log(event)
+    }
 }
