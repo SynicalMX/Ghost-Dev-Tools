1a23e1b4b0a09313871f68b339168e425a604442
SynicalMC <70176240+SynicalMC@users.noreply.github.com>
2022-11-15 10:26:33 -0500

    wip

--- src/network.ts
+++ src/network.ts
@@ -1,9 +1,26 @@
+import * as ws from 'ws';
 export default class Network {
 	private url: string
-	public socket: 
+	private id: string
+	public socket: ws.WebSocket
 	
-	constructor(url) {
+	constructor(url: string) {
 		this.url = url
+		
+		this.socket = new ws.WebSocket(url)
+
+		this.socket.once("message", (data: ws.RawData) => {
+			const msg = JSON.parse(<string><unknown>data)
+
+			if (!("method" in msg)) {
+				console.error("GDT: Received invalid network request.")
+			}
+			if (msg.method !== "sendID") {
+				console.error("GDT: Received wrong network request.")
+			}
+			
+			this.id = msg.params[0]
+		})
 	}
 
 	
